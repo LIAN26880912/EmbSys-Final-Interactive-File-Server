776da30ba95e1ebb66c4555acc1726d874ecf468
TingLin-Chu <runchu2002.cs10@nycu.edu.tw>
2024-05-27 19:10:44 +0800

    Add audio mode and keyboard interaction in main.py

--- Audio/Audio.py
+++ Audio/Audio.py
@@ -30,7 +30,7 @@ def Speech_to_Text():
         #listen for 1 seconds and create the ambient noise energy level 
         r.adjust_for_ambient_noise(source, duration=1) 
         print("Say something!")
-        audio=r.listen(source)
+        audio=r.listen(source, timeout=10)
 
     # recognize speech using Google Speech Recognition 
     try:
@@ -135,6 +135,80 @@ def yt_play_video_with_transcript(video_info):
     else:   # Failed playing
         print("Failed to play the video")
 
+
+TIME_OUT = 1
+def audio_mode(music_semaphore, locks, flags, message_queue):
+    def yt_play_video_with_transcript(video_info):
+        """ Play video with VLC Media Player (With transcript version)
+
+        Args:
+            video_info (dict): The info of the video (From yt_search_video())
+        """
+        url = video_info['link']
+        video = pafy.new(url)
+        best = video.getbest()
+        playurl = best.url
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
+        Media = Instance.media_new(playurl)
+        Media.get_mrl()
+        player.set_media(Media)
+        transcript = Transcript.get(url)['segments']
+        transcript_len = len(transcript)
+        
+        # Start playing video.
+        subscript = ""
+        i = 0
+        if player.play() == 0:   # Successful play
+            time.sleep(0.5)
+            print("----------------- Start subscript ---------------------")
+            while player.is_playing():
+                # Break or not
+                locks['end'].acquire()
+                end_flag = flags['end']
+                locks['end'].release()
+                # Access locks one-by-one to avoid deadlock
+                locks['music_stop'].acquire()
+                stop_flag = flags['music_stop']
+                locks['music_stop'].release()
+                if end_flag or stop_flag:
+                    break
+                # Continue to play music(video)
+                cur_time = player.get_time()     # In ms
+                if i < transcript_len and cur_time >= int(transcript[i]['startMs']):
+                    subscript = transcript[i]['text']
+                    message_queue.put(subscript)
+                    print(subscript)
+                    i += 1
+                # Hold for 0.1 sec
+                time.sleep(0.1)
+            print("----------------- End subscript ---------------------")
+            print("Video end")
+            # player.stop()
+        else:   # Failed playing
+            print("Failed to play the video")
+    
+    # Thread main loop
+    while True:
+        # Break or not
+        locks['end'].acquire()
+        end_flag = flags['end']
+        locks['end'].release()
+        if end_flag:
+            print("End music.")
+            break
+        # Check if the music button has been pressed
+        acked = music_semaphore.acquire(timeout=TIME_OUT)
+        if not acked:
+            continue
+        # Input a title
+        title = Speech_to_Text()
+        # Search video
+        video_info, status = yt_search_video(title)
+        if not status:
+            continue
+        # Play video
+        yt_play_video_with_transcript(video_info)
 
 if __name__ == "__main__":
     """ Testing functions. The followings are tested under windows 10, while raspi is using a linux OS. 
--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 import threading
-from queue import Queue
+from queue import Queue, Empty
 from FileServerLib import FileServerLib
 import os
 from watchdog.observers import Observer     # Ref: https://stackoverflow.com/questions/57840072/how-to-check-for-new-files-in-a-folder-in-python
 from watchdog.events import FileSystemEventHandler
 import signal
+from Audio import Audio
+import keyboard
 
-locks = {'file_created':threading.Lock(), 'file_deleted':threading.Lock(), 'end':threading.Lock(), 'falling':threading.Lock()}
-flags = {'file_created':False, 'file_deleted':False, 'end':False, 'falling':False}
+locks = {'file_created':threading.Lock(), 'file_deleted':threading.Lock(), 'end':threading.Lock(), 'falling':threading.Lock(), 'music_stop':threading.Lock()}
+flags = {'file_created':False, 'file_deleted':False, 'end':False, 'falling':False, 'music_stop':False}
 message_queue = Queue()
+music_semaphore = threading.Semaphore(0)
+TIME_OUT = 1
 
 # Define a signal handler that handle ctrl+c signal
 def end(signum, frame):
     locks['end'].acquire()
     flags['end'] = True
+    flags['music_stop'] = True
     locks['end'].release()
     print("Ending process...")
 # SIGINT means ctrl+c in linux
 signal.signal(signal.SIGINT, end)
+
+# Define a signal handler that handle start music button
+def music_start(signum, frame):
+    print("Starting music mode...")
+    music_semaphore.release()
+# SIGUSR1 means a user defined signal in linux
+signal.signal(signal.SIGUSR1, music_start)
+
+# Define a signal handler that handle music button
+def music_stop(signum, frame):
+    print("Terminating music mode...")
+    locks['music_stop'].acquire()
+    flags['music_stop'] = True
+    locks['music_stop'].release()
+# SIGUSR2 means another user defined signal in linux
+signal.signal(signal.SIGUSR2, music_stop)
+
+# Just for safe, I wrote a function works with keyboard instead of signal
+def keyboard_sense():
+    music_begin = 'y'
+    music_end = 't'
+    interrupt = 'z'
+    while True:
+        # Break or not
+        locks['end'].acquire()
+        end_flag = flags['end']
+        locks['end'].release()
+        if end_flag:
+            print("End keyboard sensing.")
+            break
+        if keyboard.is_pressed(music_begin):
+            music_start(0,0)
+        if keyboard.is_pressed(music_end):
+            music_stop(0,0)
+        if keyboard.is_pressed(interrupt):
+            end(0,0)
 
 def animation_end() -> None:
     """Not defined yet.
@@ -93,10 +134,15 @@ def display_message():
             print("End displaying message.")
             break
         # Show message (blocking with timeout for efficiency)
-        message =  message_queue.get(timeout=5)
-        show_message(message)
-        print(message)
-        message_queue.task_done()
+        try:
+            message =  message_queue.get(timeout=TIME_OUT)
+            show_message(message)
+            print(message)
+            message_queue.task_done()
+        except Empty:
+            pass
+
+        
 
 if __name__ == "__main__":
     folder_path = "."
@@ -107,9 +153,16 @@ if __name__ == "__main__":
     display_pet_thread = threading.Thread(target=display_pet)
     # Create display_text thread
     display_message_thread = threading.Thread(target=display_message)
+    # Create keyboard_sense thread
+    keyboard_sense_thread = threading.Thread(target=keyboard_sense)
+    # Create audio_mode thread
+    audio_mode_thread = threading.Thread(target=Audio.audio_mode, args=(music_semaphore, locks, flags, message_queue,))
     
     # Wait until threads end
     file_monitor_thread.join()
     display_pet_thread.join()
     display_message_thread.join()
+    keyboard_sense_thread.join()
+    audio_mode_thread.join()
+    
     print("Process terminated successfully!")
