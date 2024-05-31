de4c8e2652439ec304b56f2233e5c6a570b3d200
LIAN26880912 <sunhow123@gmail.com>
2024-05-31 14:55:57 +0800

    awtrix display update

---
+++ awtrix/index.sh
@@ -0,0 +1,3 @@
+# curl -d @notify.json -H "Content-Type: application/json" -X POST http://localhost:7000/api/v3/notify
+# curl -d @notify.json -H "Content-Type: application/json" -X POST http://localhost:7000/api/v3/notify
+curl -d @setting.json -H "Content-Type: application/json" -X POST http://localhost:7000/api/v3/settings
